4ea4248ae2ae191372fa0498e1b0dc58d591a888
Geraldo Ibere Frazao <geraldoneto39@gmail.com>
2024-03-05 13:50:07 -0300

    remove debugger

--- src/server.js
+++ src/server.js
@@ -20,8 +20,6 @@ app.use((error, request, response, next) => {
     });
   }
 
-  console.log(error)
-
   return response.status(500).json({
     status: 'error',
     message: 'Internal Server Error'
