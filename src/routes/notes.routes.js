aac0a7cef4de6603dd1b727304fb0584ad76a678
Geraldo Ibere Frazao <geraldoneto39@gmail.com>
2024-03-04 21:52:19 -0300

    notes route and all functions delete, create, show and filter working

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+
+const NotesControllers = require('../controllers/NotesController');
+
+const notesRoutes = Router();
+
+const notesControllers = new NotesControllers();
+
+notesRoutes.get('/', notesControllers.list);
+notesRoutes.post('/:user_id', notesControllers.create);
+notesRoutes.get('/:id', notesControllers.show);
+notesRoutes.delete('/:id', notesControllers.delete);
+
+module.exports = notesRoutes;
